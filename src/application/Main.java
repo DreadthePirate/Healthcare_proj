7852fb31adfdd29f0e6e2cab059d00531266f1ce
derekcdee <dcdailey@asu.edu>
2024-06-15 11:47:30 -0700

    Added initial project. Includes: Main, PatientIntake and Styles.css

---
+++ src/application/Main.java
@@ -0,0 +1,64 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+
+public class Main extends Application {
+	public static final String BUTTON_CSS_CLASS = "button-style";
+
+    @Override
+    public void start(Stage primaryStage) {
+    	 VBox vbox = new VBox(10);
+         vbox.setAlignment(Pos.CENTER);
+
+         // Title
+         Text title = new Text("Welcome to Pediatric Kids Office System!");
+         title.setFont(Font.font("Arial", 18));
+         VBox titleBox = new VBox(title);
+         titleBox.setAlignment(Pos.CENTER);
+         titleBox.setPadding(new Insets(0, 0, 10, 0));
+         
+         // Buttons
+         Button btnIntake = new Button("Intake Portal");
+         Button btnPatient = new Button("Patient Portal");
+         Button btnDoctor = new Button("Doctor Portal");
+
+         // Styling buttons
+         btnIntake.getStyleClass().add(BUTTON_CSS_CLASS);
+         btnDoctor.getStyleClass().add(BUTTON_CSS_CLASS);
+         btnPatient.getStyleClass().add(BUTTON_CSS_CLASS);
+
+         // Button events
+         btnIntake.setOnAction(e -> new PatientIntake().start(primaryStage));
+         btnDoctor.setOnAction(e -> System.out.println("Doctor Portal clicked"));
+         btnPatient.setOnAction(e -> System.out.println("Patient Portal clicked"));
+
+         // Adding components to the VBox
+         vbox.getChildren().addAll(titleBox, btnIntake, btnPatient, btnDoctor);
+
+         // Set up the Scene
+         Scene scene = new Scene(vbox, 500, 325);
+         scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
+
+         // Set the stage
+         primaryStage.setTitle("Pediatric Kids Office");
+         primaryStage.setScene(scene);
+         primaryStage.show();
+    }
+
+    // Class functions here
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/application/PatientIntake.java
@@ -0,0 +1,93 @@
+package application;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class PatientIntake extends Application {
+	public static final String BUTTON_SMALL_CSS_CLASS = "button-small-style";
+
+    @Override
+    public void start(Stage primaryStage) {
+    	primaryStage.setTitle("Patient Intake Form");
+
+        // Main layout is a VBox
+        VBox mainLayout = new VBox(10);
+        mainLayout.setPadding(new Insets(10, 10, 10, 10));
+
+        // Go Back button
+        Button btnGoBack = new Button("Go Back");
+        HBox topControls = new HBox(btnGoBack);
+        topControls.setAlignment(Pos.TOP_LEFT);
+
+        // Title
+        Text title = new Text("Patient Intake Form");
+        title.setFont(Font.font("Arial", 18));
+        HBox titleBox = new HBox(title);
+        titleBox.setAlignment(Pos.CENTER);
+        titleBox.setPadding(new Insets(20, 0, 25, 0));
+
+        // Grid for the form fields
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(30);
+        grid.setVgap(20);
+        grid.add(new Label("First Name:"), 0, 0);
+        grid.add(new TextField(), 1, 0);
+        grid.add(new Label("Last Name:"), 0, 1);
+        grid.add(new TextField(), 1, 1);
+        grid.add(new Label("Date Of Birth:"), 0, 2);
+        grid.add(new TextField(), 1, 2);
+        grid.add(new Label("Weight:"), 0, 3);
+        grid.add(new TextField(), 1, 3);
+        grid.add(new Label("Height:"), 0, 4);
+        grid.add(new TextField(), 1, 4);
+        grid.add(new Label("Blood Pressure:"), 0, 5);
+        grid.add(new TextField(), 1, 5);
+        grid.add(new Label("Allergies:"), 0, 6);
+        grid.add(new TextField(), 1, 6);
+        grid.add(new Label("Health Concerns:"), 0, 7);
+        grid.add(new TextField(), 1, 7);
+        VBox.setMargin(grid, new Insets(0, 0, 40, 0));
+
+        // Save button
+        Button btnSave = new Button("Save");
+        HBox bottomControls = new HBox(btnSave);
+        bottomControls.setAlignment(Pos.BOTTOM_RIGHT);
+        
+        // Styling buttons
+        btnGoBack.getStyleClass().add(BUTTON_SMALL_CSS_CLASS);
+        btnSave.getStyleClass().add(BUTTON_SMALL_CSS_CLASS);
+        
+        // Button events
+        btnGoBack.setOnAction(e -> new Main().start(primaryStage));
+        btnSave.setOnAction(e -> System.out.println("Save clicked"));
+
+        // Adding components to the VBox
+        mainLayout.getChildren().addAll(topControls, titleBox, grid, bottomControls);
+        
+        // Set up the Scene
+        Scene scene = new Scene(mainLayout, 475, 600);
+        scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
+        
+        // Set the stage
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    //Class functions here
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
