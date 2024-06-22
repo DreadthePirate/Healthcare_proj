9276a8940970e70262cf3650c5da0bdc063fbf59
Devinj48 <drjiang1@asu.edu>
2024-06-22 10:35:20 -0700

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -13,50 +13,47 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 
 public class Main extends Application {
-	public static final String BUTTON_CSS_CLASS = "button-style";
+    public static final String BUTTON_CSS_CLASS = "button-style";
 
     @Override
     public void start(Stage primaryStage) {
-    	 VBox vbox = new VBox(10);
-         vbox.setAlignment(Pos.CENTER);
+        VBox vbox = new VBox(10);
+        vbox.setAlignment(Pos.CENTER);
 
-         // Title
-         Text title = new Text("Welcome to Pediatric Kids Office System!");
-         title.setFont(Font.font("Arial", 18));
-         VBox titleBox = new VBox(title);
-         titleBox.setAlignment(Pos.CENTER);
-         titleBox.setPadding(new Insets(0, 0, 10, 0));
-         
-         // Buttons
-         Button btnIntake = new Button("Intake Portal");
-         Button btnPatient = new Button("Patient Portal");
-         Button btnDoctor = new Button("Doctor Portal");
+        // Title
+        Text title = new Text("Welcome to Pediatric Kids Office System!");
+        title.setFont(Font.font("Arial", 18));
+        VBox titleBox = new VBox(title);
+        titleBox.setAlignment(Pos.CENTER);
+        titleBox.setPadding(new Insets(0, 0, 10, 0));
 
-         // Styling buttons
-         btnIntake.getStyleClass().add(BUTTON_CSS_CLASS);
-         btnDoctor.getStyleClass().add(BUTTON_CSS_CLASS);
-         btnPatient.getStyleClass().add(BUTTON_CSS_CLASS);
+        // Buttons
+        Button btnIntake = new Button("Intake Portal");
+        Button btnPatient = new Button("Patient Portal");
+        Button btnDoctor = new Button("Doctor Portal");
 
-         // Button events
-         btnIntake.setOnAction(e -> new PatientIntake().start(primaryStage));
-         btnDoctor.setOnAction(e -> new DoctorLogin().start(primaryStage));
-         btnPatient.setOnAction(e -> System.out.println("Patient Portal clicked"));
+        // Styling buttons
+        btnIntake.getStyleClass().add(BUTTON_CSS_CLASS);
+        btnDoctor.getStyleClass().add(BUTTON_CSS_CLASS);
+        btnPatient.getStyleClass().add(BUTTON_CSS_CLASS);
 
-         // Adding components to the VBox
-         vbox.getChildren().addAll(titleBox, btnIntake, btnPatient, btnDoctor);
+        // Button events
+        btnIntake.setOnAction(e -> new PatientIntake().start(primaryStage));
+        btnDoctor.setOnAction(e -> new DoctorLogin().start(primaryStage));
+        btnPatient.setOnAction(e -> new PatientLogin().start(primaryStage));
 
-         // Set up the Scene
-         Scene scene = new Scene(vbox, 500, 325);
-         scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
+        // Adding components to the VBox
+        vbox.getChildren().addAll(titleBox, btnIntake, btnPatient, btnDoctor);
 
-         // Set the stage
-         primaryStage.setTitle("Pediatric Kids Office");
-         primaryStage.setScene(scene);
-         primaryStage.show();
+        // Set up the Scene
+        Scene scene = new Scene(vbox, 500, 325);
+        scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
+
+        // Set the stage
+        primaryStage.setTitle("Pediatric Kids Office");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
-    // Class functions here
-
 
     public static void main(String[] args) {
         launch(args);
